f7fd93cd0459d4eeda90bd7713dfa7a25279dd71
Damon Ba <damonba@eduroam-269-2-095.epfl.ch>
2025-03-09 02:00:24 +0100

    backend smartcontract

--- backend/proof-verifier/src/lib.rs
+++
@@ -1,6 +0,0 @@
-use wasm_bindgen::prelude::wasm_bindgen;
-
-#[wasm_bindgen]
-pub fn verify_proof(proof: &str, verification_key: &str, restaurant_public_key: &str, time_range_start: i64, time_range_end: i64) -> bool {
-    true // Mock, à remplacer par la logique réelle
-}
